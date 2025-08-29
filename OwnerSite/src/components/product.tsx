964170944ff3285756f9dfef2c28c0459f8267d2
Subham Mondal <subhamoffi@gmail.com>
2025-08-29 11:31:42 +0530

    taking data from backend

--- OwnerSite/src/components/product.tsx
+++ OwnerSite/src/components/product.tsx
@@ -32,57 +32,55 @@ const Product = ({
   };
 
   return (
-    Images.length > 0 && (
-      <div className="h-full snap-center w-50 mx-auto shrink-0 flex justify-center flex-col gap-2">
-        <div className="h-40 flex-1 rounded-md w-full relative">
-          <div className="absolute top-2 h-6 text-white font-bold w-12 bg-black rounded-md text-center right-2">
-            {Math.floor(
-              ((productAcutalPrice - productDiscountedPrice) /
-                productAcutalPrice) *
-                100
-            )}
-            % off
-          </div>
-          <img
-            onClick={() => {
-              navigate(`/edit/product/${productId}`);
-            }}
-            src={Images[0].imgUrl}
-            alt="this is alt"
-            className="h-full w-full object-cover rounded-md"
-          />
+    <div className="h-full snap-center w-50 mx-auto shrink-0 flex justify-center flex-col gap-2">
+      <div className="h-40 flex-1 rounded-md w-full relative">
+        <div className="absolute top-2 h-6 text-white font-bold w-12 bg-black rounded-md text-center right-2">
+          {Math.floor(
+            ((productAcutalPrice - productDiscountedPrice) /
+              productAcutalPrice) *
+              100
+          )}
+          % off
         </div>
-        <div className="font-toreadore">
-          Name :{" "}
-          {productName.length > 1
-            ? `${productName.slice(0, 10)}...`
-            : productName}
-          <br />
-          Price :{" "}
-          <span className="text-xs line-through text-slate-500">
-            {productAcutalPrice}
-          </span>
-          <span className="font-bold ml-2">₹{productDiscountedPrice}</span>
-        </div>
-        <div className="flex justify-between">
-          <button
-            className="bg-red-500 text-white rounded-md hover:bg-transparent hover:border border-black hover:text-black transition-all duration-300 px-5 py-2 font-toreadore"
-            onClick={handleDelete}
-          >
-            Delete
-          </button>
-          <button
-            className="bg-green-500 text-white rounded-md hover:bg-transparent hover:border border-black hover:text-black transition-all duration-300 px-5 py-2 font-toreadore"
-            onClick={(e) => {
-              e.stopPropagation();
-              navigate(`/edit/product/${productId}`);
-            }}
-          >
-            Edit
-          </button>
-        </div>
+        <img
+          onClick={() => {
+            navigate(`/edit/product/${productId}`);
+          }}
+          src={Images[0].imgUrl}
+          alt="this is alt"
+          className="h-full w-full object-cover rounded-md"
+        />
       </div>
-    )
+      <div className="font-toreadore">
+        Name :{" "}
+        {productName.length > 1
+          ? `${productName.slice(0, 10)}...`
+          : productName}
+        <br />
+        Price :{" "}
+        <span className="text-xs line-through text-slate-500">
+          {productAcutalPrice}
+        </span>
+        <span className="font-bold ml-2">₹{productDiscountedPrice}</span>
+      </div>
+      <div className="flex justify-between">
+        <button
+          className="bg-red-500 text-white rounded-md hover:bg-transparent hover:border border-black hover:text-black transition-all duration-300 px-5 py-2 font-toreadore"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+        <button
+          className="bg-green-500 text-white rounded-md hover:bg-transparent hover:border border-black hover:text-black transition-all duration-300 px-5 py-2 font-toreadore"
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate(`/edit/product/${productId}`);
+          }}
+        >
+          Edit
+        </button>
+      </div>
+    </div>
   );
 };
 
@@ -99,57 +97,55 @@ export const FilterProduct = ({
   };
 
   return (
-    Images.length > 0 && (
-      <div className="h-full snap-center sm:w-40 md:w-58 mx-auto shrink-0 flex justify-center flex-col gap-2">
-        <div className="h-40 flex-1 rounded-md w-full relative">
-          <div className="absolute top-2 h-6 text-white font-bold w-12 bg-black rounded-md text-xs flex items-center justify-center right-2">
-            {Math.floor(
-              ((productAcutalPrice - productDiscountedPrice) /
-                productAcutalPrice) *
-                100
-            )}
-            % off
-          </div>
-          <img
-            onClick={() => {
-              navigate(`/edit/product/${productId}`);
-            }}
-            src={Images[0].imgUrl}
-            alt="this is alt"
-            className="h-full w-full object-cover rounded-md"
-          />
+    <div className="h-full snap-center sm:w-40 md:w-58 mx-auto shrink-0 flex justify-center flex-col gap-2">
+      <div className="h-40 flex-1 rounded-md w-full relative">
+        <div className="absolute top-2 h-6 text-white font-bold w-12 bg-black rounded-md text-xs flex items-center justify-center right-2">
+          {Math.floor(
+            ((productAcutalPrice - productDiscountedPrice) /
+              productAcutalPrice) *
+              100
+          )}
+          % off
         </div>
-        <div className="font-toreadore">
-          Name :{" "}
-          {productName.length > 1
-            ? `${productName.slice(0, 10)}...`
-            : productName}
-          <br />
-          Price :{" "}
-          <span className="text-xs line-through text-slate-500">
-            {productAcutalPrice}
-          </span>
-          <span className="font-bold ml-2">₹{productDiscountedPrice}</span>
-        </div>
-        <div className="flex justify-between">
-          <button
-            className="bg-red-500 text-white rounded-md hover:bg-transparent hover:border border-black hover:text-black transition-all duration-300 px-5 py-2 font-toreadore"
-            onClick={handleDelete}
-          >
-            Delete
-          </button>
-          <button
-            className="bg-green-500 text-white rounded-md hover:bg-transparent hover:border border-black hover:text-black transition-all duration-300 px-5 py-2 font-toreadore"
-            onClick={(e) => {
-              e.stopPropagation();
-              navigate(`/edit/product/${productId}`);
-            }}
-          >
-            Edit
-          </button>
-        </div>
+        <img
+          onClick={() => {
+            navigate(`/edit/product/${productId}`);
+          }}
+          src={Images[0].imgUrl}
+          alt="this is alt"
+          className="h-full w-full object-cover rounded-md"
+        />
       </div>
-    )
+      <div className="font-toreadore">
+        Name :{" "}
+        {productName.length > 1
+          ? `${productName.slice(0, 10)}...`
+          : productName}
+        <br />
+        Price :{" "}
+        <span className="text-xs line-through text-slate-500">
+          {productAcutalPrice}
+        </span>
+        <span className="font-bold ml-2">₹{productDiscountedPrice}</span>
+      </div>
+      <div className="flex justify-between">
+        <button
+          className="bg-red-500 text-white rounded-md hover:bg-transparent hover:border border-black hover:text-black transition-all duration-300 px-5 py-2 font-toreadore"
+          onClick={handleDelete}
+        >
+          Delete
+        </button>
+        <button
+          className="bg-green-500 text-white rounded-md hover:bg-transparent hover:border border-black hover:text-black transition-all duration-300 px-5 py-2 font-toreadore"
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate(`/edit/product/${productId}`);
+          }}
+        >
+          Edit
+        </button>
+      </div>
+    </div>
   );
 };
 
